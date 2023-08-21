fd3352b8f7ab41e88fee14a8a4bb18eddba0d461
Uzair Saleem <uzairsaleemdev@gmail.com>
2023-08-21 16:19:25 +0500

    frontend of socket.io implemented in react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import styles from "./App.module.css"
-import SocketConnection from "./components/SocketConnection/SocketConnection";
+import { socket } from "./socket";
+import { useState, useEffect} from 'react'
+import {ConnectionState} from './components/ConnectionState'
+import {ConnectionManager} from './components/ConnectionManager'
+import {MyForm} from './components/MyForm'
+import { Events } from "./components/Events";
 function App() {
+
+  const [isConnected, setIsConnected] = useState(socket.connected)
+  const [fooEvents,setFooEvents] = useState([])
+  useEffect(() => {
+    function onConnect(){
+      setIsConnected(true)
+    }
+    function onDisconnect(){
+      setIsConnected(false)
+    }
+    function onFooEvent (value){
+          setFooEvents(prev=>[...prev,value])
+    }
+    socket.on('connect',onConnect)
+    socket.on('disconnect',onDisconnect)
+    socket.on('foo',onFooEvent)
+  
+    return () => {
+      socket.off('connect',onConnect)
+      socket.off('disconnect',onDisconnect)
+      socket.off('foo',onFooEvent)
+    }
+  }, [])
   
   return (
+
+
     <div className={styles.app}>
-      <h1>Hello there</h1>
-      <SocketConnection/>
+      <ConnectionState isConnected={isConnected} />
+      <Events events={fooEvents}/>
+      <ConnectionManager  />
+      <MyForm/>
     </div>
 
   )
